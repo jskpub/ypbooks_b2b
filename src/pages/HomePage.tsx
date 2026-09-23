aeccf6f18b2ff8ad5f527208913b2c1c790e6402
jsk <jsk.pubwork@gmail.com>
2026-09-24 04:21:19 +0900

    [update] 안내 문구 추가

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,9 @@ export default function HomePage() {
     <main id='main' className='main home'>
       <div className='container' style={{ height: '1000px' }}>
         <h1>공통레이아웃 (확인용 높이(1000px) 설정- 실제 코드에 사용되지 않음)</h1>
+
+        <br />
+        <h2>src/pages에 파일 추가하여 작업 진행해주세요</h2>
       </div>
     </main>
   );
