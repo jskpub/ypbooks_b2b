552ac246867a8ddc74504771cf6388b5a3e021e6
jsk <jsk.pubwork@gmail.com>
2026-09-24 04:23:42 +0900

    [update] 안내 문구 추가

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
         <h1>공통레이아웃 (확인용 높이(1000px) 설정- 실제 코드에 사용되지 않음)</h1>
 
         <br />
-        <h2>src/pages에 파일 추가하여 작업 진행해주세요</h2>
+        <h2>src/pages에 파일 추가하여 작업 진행</h2>
       </div>
     </main>
   );
