da507f80925fc0a51b28ff4bd4bc4266cb8aa69b
jsk <jsk.pubwork@gmail.com>
2026-09-24 17:52:49 +0900

    [update] 기본 레이아웃 home 클래스 제거

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 export default function HomePage() {
   return (
-    <main id='main' className='main home'>
+    <main id='main' className='main'>
       <div className='container' style={{ height: '1000px' }}>
         <h1>공통레이아웃 (확인용 높이(1000px) 설정- 실제 코드에 사용되지 않음)</h1>
 
